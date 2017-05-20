1fb199005aafa5d636ae12a910f6dde07d093623
Danara <danaramooon@gmail.com>
2017-05-20 10:00:14 +0600

    10 : 00

--- FINAL_1/FINAL_2/Form1.cs
+++ FINAL_1/FINAL_2/Form1.cs
@@ -12,9 +12,64 @@ namespace FINAL_2
 {
     public partial class Form1 : Form
     {
+        public int x = 0, y = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+        public bool is_prime(int x)
+        {
+            if (x == 1 | x == 0)
+                return false;
+            for (int i = 2; i < x; i++)
+            {
+                if (x % i == 0)
+                    return true;
+            }
+            return true;
+        }
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            btn.Text += x;
+            x++;
+            btn.Text = btn.Text;
+            if (is_prime(int.Parse(btn.Text)))
+            {
+                textBox1.Text += y;
+                y++;
+            }
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
